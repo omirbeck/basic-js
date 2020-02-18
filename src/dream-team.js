fae671dc54b1462c830c85440432852c301ab115
omirbeck <ommirtull@gmail.com>
2020-02-18 11:35:15 +0600

    fix: complete dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,15 +1,13 @@
 module.exports = function createDreamTeam(members) {
-  if (typeof(members) !== 'object' || members == null || members == { }) {
-    return false 
-  } else { 
-  const result = members.filter(index => typeof(index) == 'string')
-  const dream = result.map(index => {
+  if (members instanceof Array) {
+    const result = members.filter(index => typeof(index) == 'string')
+    const dream = result.map(index => {
     let dreams = String(index.trim());
     return dreams[0].toUpperCase();
   })
     return dream.sort().join('');
   }
- 
-  //throw 'Not implemented';
-  // remove line with error and write your code here
+  else { 
+    return false; 
+  }
 };
