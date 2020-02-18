910bf8314c748b3b393b241dc15413b195a1f9c8
omirbeck <ommirtull@gmail.com>
2020-02-18 10:56:26 +0600

    feat: add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  if (typeof(members) !== 'object' || members == null || members == { }) {
+    return false 
+  } else { 
+  const result = members.filter(index => typeof(index) == 'string')
+  const dream = result.map(index => {
+    let dreams = String(index.trim());
+    return dreams[0].toUpperCase();
+  })
+    return dream.sort().join('');
+  }
+ 
+  //throw 'Not implemented';
   // remove line with error and write your code here
 };
