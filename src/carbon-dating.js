ef79bee7b711ffbaccac7f3e17aa0b9e6fd856e4
omirbeck <ommirtull@gmail.com>
2020-02-19 17:53:29 +0600

    feat: carbon-dating check for type

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity instanceof String) {
+
+  } else return false
+  //throw 'Not implemented';
   // remove line with error and write your code here
 };
