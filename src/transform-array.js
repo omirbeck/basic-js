8f44d6e50848419b09ffb9aae90a62035ef2413a
omirbeck <ommirtull@gmail.com>
2020-03-04 17:59:39 +0600

    feat: transform array double-next

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,17 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
+    if (arr instanceof Array) {
+        let array = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == '--double-next') {
+                array.push(arr[i + 1])
+                console.log(array)
+            } else if (arr[i] == '--discard-next') {
+                i++;
+            } else {
+                array.push(arr[i]);
+            }
+        } return array;
+    } else throw Error;   
+    //throw 'Not implemented';
     // remove line with error and write your code here
 };
