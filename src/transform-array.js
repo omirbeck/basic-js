353bf2312f4435388dc1dda4b0fdd35d79299655
omirbeck <ommirtull@gmail.com>
2020-03-07 00:11:39 +0600

    feat: transform task transformed array

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,14 +4,20 @@ module.exports = function transform(arr) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] == '--double-next') {
                 array.push(arr[i + 1])
-                console.log(array)
             } else if (arr[i] == '--discard-next') {
                 i++;
-            } else {
+            } else if (arr[i] == '--discard-prev') {
+                array.splice(array.length-1,1);
+            } else if (arr[i] == '--double-prev') {
+                array.push(array[i-1])
+            }
+            else {
                 array.push(arr[i]);
             }
         } return array;
-    } else throw Error;   
+    } else throw Error;    
     //throw 'Not implemented';
     // remove line with error and write your code here
 };
+
+//console.log(transform(arr))
