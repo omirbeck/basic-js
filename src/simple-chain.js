14b59e16c5f1864b2d0d80469bcaf7a527de3a66
omirbeck <ommirtull@gmail.com>
2020-03-22 16:12:07 +0600

    feat: simple chain compleate

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ((typeof position === 'number') && (position > 0) && (position <= this.arr.length)) {
+      this.arr.splice(position - 1,1);
+      return this;
+    } else {
+      this.arr = [];
+      throw Error;
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr = this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chain = this.arr.join('~~');
+    this.arr = [];
+    return chain;
   }
 };
 
